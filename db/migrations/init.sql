83f312bbc266784512901737a340bfb8d29d4c3d
Yihao Jiang <yihao.jiang@merico.dev>
2022-04-09 23:46:09 +0800

    fix: avatar pic paths

--- db/migrations/init.sql
+++ db/migrations/init.sql
@@ -20,19 +20,19 @@ CREATE TABLE IF NOT EXISTS "upvote" (
 
 -- create some users
 INSERT INTO "user" (name, avatar_path)
-VALUES('Robe Hope', 'avatar_01.jpg'),
-('Sophie Brecht', 'avatar_02.jpg'),
-('Cameron Lawrence', 'avatar_03.jpg'),
-('Elon Musk', 'avatar_04.jpg'),
-('Bill Gates', 'avatar_05.jpg'),
-('Yihao Jiang', 'avatar_06.jpg'),
-('Jeff Bezos', 'avatar_07.jpg'),
-('Will Smith', 'avatar_08.jpg'),
-('Chris Rock', 'avatar_09.jpg'),
-('Ellen DeGeneres', 'avatar_10.jpg');
+VALUES('Robe Hope', '../assets/avatars/avatar_01.png'),
+('Sophie Brecht', '../assets/avatars/avatar_02.png'),
+('Cameron Lawrence', '../assets/avatars/avatar_03.png'),
+('Elon Musk', '../assets/avatars/avatar_04.png'),
+('Bill Gates', '../assets/avatars/avatar_05.png'),
+('Yihao Jiang', '../assets/avatars/avatar_06.png'),
+('Jeff Bezos', '../assets/avatars/avatar_07.png'),
+('Will Smith', '../assets/avatars/avatar_08.png'),
+('Chris Rock', '../assets/avatars/avatar_09.png'),
+('Ellen DeGeneres', '../assets/avatars/avatar_10.png');
 
 -- create some comments
 INSERT INTO "comment" (parent_id, content, author_id, create_time)
-VALUES(null, 'Jeepers now that''s huge release with some big community earnings to back it - it must be so rewarding seeing creators quit their day jobs after monetizing (with real MRR) on the new platform.', 0, '2022-01-01 16:00:00+08'),
-(null, 'Switched our blog from Hubspot to Ghost a year ago -- turned out to be a great decision. Looking forward to this update....the in-platform analytics look especially delicious.:)', 1, '2022-01-02 16:00:00+08'),
-(null, 'Love the native memberships and the zipless themes, I was just asked by a friend about options for a new site, and I think I know what I''ll be recommending then...', 2, '2022-03-01 16:00:00+08');
+VALUES (null, 'Jeepers now that''s huge release with some big community earnings to back it - it must be so rewarding seeing creators quit their day jobs after monetizing (with real MRR) on the new platform.', 1, '2022-01-01 16:00:00+08'),
+(null, 'Switched our blog from Hubspot to Ghost a year ago -- turned out to be a great decision. Looking forward to this update....the in-platform analytics look especially delicious.:)', 2, '2022-01-02 16:00:00+08'),
+(null, 'Love the native memberships and the zipless themes, I was just asked by a friend about options for a new site, and I think I know what I''ll be recommending then...', 3, '2022-03-01 16:00:00+08');
